e8185ab7a6b503cdb0e14763b3bedeca23daa6ec
Brian <Brian@192.168.1.123>
2012-11-04 14:52:46 -0600

    Finished Project.  Has a Gui, startup class and Guessing Game service.

---
+++ src/adh/guessinggame/GuessingGameService.java
@@ -0,0 +1,41 @@
+package adh.guessinggame;
+import java.util.Random;
+
+/**
+ *
+ * @Drew Holloway
+ */
+public class GuessingGameService {
+    private int correctNumber;
+    private Random generator;
+    private int minGuess;
+    private int maxGuess;
+    
+    public GuessingGameService(int minGuess, int maxGuess){
+        generator = new Random(System.nanoTime());
+        this.minGuess=minGuess;
+        this.maxGuess=maxGuess;
+    }
+    
+    public void setCorrectNumber(){
+        correctNumber=generator.nextInt(maxGuess-minGuess+1)+minGuess;
+    }
+    
+    public int testGuess(int guess){
+        if (guess<correctNumber){
+            // guess was too low, return -1
+            return -1;
+        } else if (guess>correctNumber){
+            // guess was too high, return 1
+            return 1;
+        } else {
+            // guess was correct, return 0
+            return 0;
+        }
+    }
+        
+    
+    public int getCorrectNumber(){
+        return correctNumber;
+    }
+}
